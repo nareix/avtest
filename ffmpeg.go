296562f8eabe46dbe8eda6b3596bdd91d6f7e4d1
nareix <hi.nareix@gmail.com>
2016-04-19 21:29:03 +0800

    fix ffmpeg bug

--- ffmpeg.go
+++ ffmpeg.go
@@ -6,9 +6,12 @@ package main
 #cgo CFLAGS: -Werror
 #include <libavformat/avformat.h>
 
+const int AAC = CODEC_ID_AAC;
+const int H264 = CODEC_ID_H264;
+
 typedef struct {
 	AVFormatContext *fmt_ctx;
-	AVPacket pkt;
+	AVPacket *pkt;
 } ffctx_t;
 
 void ffctx_openfile(ffctx_t *ctx, const char *filename) {
@@ -17,47 +20,72 @@ void ffctx_openfile(ffctx_t *ctx, const char *filename) {
 	//av_dump_format(ctx->fmt_ctx, 0, filename, 0);
 	AVPacket pkt;
 
-	#if 0
 	for (int i = 0; i < ctx->fmt_ctx->nb_streams; i++) {
 		AVStream *stream = ctx->fmt_ctx->streams[i];
-		printf("stream=%d extradata_size=%d\n", i, stream->codec->extradata_size);
 	}
 
-	int i = 0;
-	while (av_read_frame(ctx->fmt_ctx, &pkt) >= 0) {
-		//printf("i=%d stream_index=%d\n", i, pkt.stream_index);
-		av_free_packet(&pkt);
-		i++;
-	}
-	#endif
+	ctx->pkt = malloc(sizeof(AVPacket));
 }
 
-void ffctx_get_stream_codec_extradata(ffctx_t *ctx, int i, void **p, int *len) {
+void ffctx_close(ffctx_t *ctx) {
+	free(ctx->pkt);
+	avformat_close_input(&ctx->fmt_ctx);
+}
+
+void ffctx_get_stream_codec_extradata(ffctx_t *ctx, int i, void **p, int *len, int *p_id) {
 	AVStream *stream = ctx->fmt_ctx->streams[i];
 	*p = stream->codec->extradata;
 	*len = stream->codec->extradata_size;
+	*p_id = stream->codec->codec_id;
 }
 
 void ffctx_read_frame(ffctx_t *ctx) {
-	av_read_frame(ctx->fmt_ctx, &ctx->pkt);
+	av_read_frame(ctx->fmt_ctx, ctx->pkt);
 }
 
 void ffctx_free_packet(ffctx_t *ctx) {
-	//av_free_packet(&ctx->pkt);
+	av_free_packet(ctx->pkt);
 }
 
 */
 import "C"
-import "unsafe"
+
+import (
+	"unsafe"
+	"fmt"
+	_ "github.com/nareix/av"
+)
+
+type FFMpegStream struct {
+}
 
 func init() {
 	ctx := &C.ffctx_t{}
 	var p unsafe.Pointer
-	var size C.int
+	var size, id C.int
 
-	C.ffctx_openfile(ctx, C.CString("test.mp4"))
-	C.ffctx_get_stream_codec_extradata(ctx, 0, &p, &size)
-	b := C.GoBytes(p, size)
-	println("ffmpeg", b)
+	fmt.Printf("ffmpeg: AAC=%d\n", C.AAC)
+	fmt.Printf("ffmpeg: H264=%d\n", C.H264)
+
+	C.ffctx_openfile(ctx, C.CString("projectindex.mp4"))
+
+	C.ffctx_get_stream_codec_extradata(ctx, 0, &p, &size, &id)
+	fmt.Println("ffmpeg: CodecData size", size)
+
+	C.ffctx_get_stream_codec_extradata(ctx, 1, &p, &size, &id)
+	fmt.Println("ffmpeg: CodecData size", size)
+
+	C.ffctx_get_stream_codec_extradata(ctx, 1, &p, &size, &id)
+	fmt.Println("ffmpeg: CodecData size", size)
+
+	C.ffctx_read_frame(ctx)
+	C.ffctx_free_packet(ctx)
+	C.ffctx_read_frame(ctx)
+	C.ffctx_free_packet(ctx)
+	C.ffctx_read_frame(ctx)
+	C.ffctx_free_packet(ctx)
+	C.ffctx_read_frame(ctx)
+	C.ffctx_free_packet(ctx)
 }
+
 
