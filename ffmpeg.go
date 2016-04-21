714bcfa8e2e52e35c175a9f7a7943572c4a1ef67
nareix <hi.nareix@gmail.com>
2016-04-21 21:20:34 +0800

    imporve ffmpeg.go

--- ffmpeg.go
+++ ffmpeg.go
@@ -14,17 +14,12 @@ typedef struct {
 	AVPacket *pkt;
 } ffctx_t;
 
-void ffctx_openfile(ffctx_t *ctx, const char *filename) {
+void ffctx_openfile(ffctx_t *ctx, const char *filename, int *n) {
 	av_register_all();
 	avformat_open_input(&ctx->fmt_ctx, filename, NULL, NULL);
 	//av_dump_format(ctx->fmt_ctx, 0, filename, 0);
-	AVPacket pkt;
-
-	for (int i = 0; i < ctx->fmt_ctx->nb_streams; i++) {
-		AVStream *stream = ctx->fmt_ctx->streams[i];
-	}
-
 	ctx->pkt = malloc(sizeof(AVPacket));
+	*n = ctx->fmt_ctx->nb_streams;
 }
 
 void ffctx_close(ffctx_t *ctx) {
@@ -32,11 +27,12 @@ void ffctx_close(ffctx_t *ctx) {
 	avformat_close_input(&ctx->fmt_ctx);
 }
 
-void ffctx_get_stream_codec_extradata(ffctx_t *ctx, int i, void **p, int *len, int *p_id) {
+void ffctx_get_stream_codec_extradata(ffctx_t *ctx, int i, void **p, int *len, int *p_id, int64_t *time_scale) {
 	AVStream *stream = ctx->fmt_ctx->streams[i];
 	*p = stream->codec->extradata;
 	*len = stream->codec->extradata_size;
 	*p_id = stream->codec->codec_id;
+	*time_scale = stream->time_base.den;
 }
 
 void ffctx_read_frame(ffctx_t *ctx) {
@@ -46,46 +42,86 @@ void ffctx_read_frame(ffctx_t *ctx) {
 void ffctx_free_packet(ffctx_t *ctx) {
 	av_free_packet(ctx->pkt);
 }
-
 */
 import "C"
 
 import (
 	"unsafe"
-	"fmt"
-	_ "github.com/nareix/av"
+	"github.com/nareix/av"
 )
 
-type FFMpegStream struct {
+type FFStream struct {
+	av.StreamCommon
+	timeScale int64
+}
+
+type FFDemuxer struct {
+	Filename string
+	time float64
+	streams []*FFStream
+	ctx *C.ffctx_t
+}
+
+func (self *FFDemuxer) Streams() (streams []av.Stream) {
+	for _, stream := range(self.streams) {
+		streams = append(streams, stream)
+	}
+	return
+}
+
+func (self *FFDemuxer) ReadHeader() (err error) {
+	var streamNr C.int
+
+	self.ctx = &C.ffctx_t{}
+	C.ffctx_openfile(self.ctx, C.CString(self.Filename), &streamNr)
+
+	for i := 0; i < int(streamNr); i++ {
+		var p unsafe.Pointer
+		var size, id C.int
+		var timeScale C.int64_t
+		stream := &FFStream{}
+		C.ffctx_get_stream_codec_extradata(self.ctx, C.int(i), &p, &size, &id, &timeScale)
+		stream.timeScale = int64(timeScale)
+		b := C.GoBytes(p, size)
+		switch id {
+		case C.H264:
+			stream.SetType(av.H264)
+		case C.AAC:
+			stream.SetType(av.AAC)
+		}
+		if err = stream.SetCodecData(b); err != nil {
+			return
+		}
+		self.streams = append(self.streams, stream)
+	}
+
+	return
+}
+
+func (self *FFDemuxer) SeekToTime(time float64) (err error) {
+	C.av_seek_frame(self.ctx.fmt_ctx, -1, C.int64_t(time*float64(C.AV_TIME_BASE)), 0)
+	return
+}
+
+func (self *FFDemuxer) Time() float64 {
+	return self.time
+}
+
+func (self *FFDemuxer) ReadPacket() (i int, pkt av.Packet, err error) {
+	C.ffctx_read_frame(self.ctx)
+	cpkt := self.ctx.pkt
+	i = int(cpkt.stream_index)
+	stream := self.streams[i]
+	self.time = float64(cpkt.dts)/float64(stream.timeScale)
+	pkt.Data = C.GoBytes(unsafe.Pointer(cpkt.data), cpkt.size)
+	pkt.Duration = float64(cpkt.duration)/float64(stream.timeScale)
+	pkt.CompositionTime = float64(cpkt.pts-cpkt.dts)/float64(stream.timeScale)
+	pkt.IsKeyFrame = cpkt.flags&C.AV_PKT_FLAG_KEY!=0
+	C.ffctx_free_packet(self.ctx)
+	return
 }
 
 func init() {
-	ctx := &C.ffctx_t{}
-	var p unsafe.Pointer
-	var size, id C.int
-
-	fmt.Printf("ffmpeg: AAC=%d\n", C.AAC)
-	fmt.Printf("ffmpeg: H264=%d\n", C.H264)
-
-	C.ffctx_openfile(ctx, C.CString("projectindex.mp4"))
-
-	C.ffctx_get_stream_codec_extradata(ctx, 0, &p, &size, &id)
-	fmt.Println("ffmpeg: CodecData size", size)
-
-	C.ffctx_get_stream_codec_extradata(ctx, 1, &p, &size, &id)
-	fmt.Println("ffmpeg: CodecData size", size)
-
-	C.ffctx_get_stream_codec_extradata(ctx, 1, &p, &size, &id)
-	fmt.Println("ffmpeg: CodecData size", size)
-
-	C.ffctx_read_frame(ctx)
-	C.ffctx_free_packet(ctx)
-	C.ffctx_read_frame(ctx)
-	C.ffctx_free_packet(ctx)
-	C.ffctx_read_frame(ctx)
-	C.ffctx_free_packet(ctx)
-	C.ffctx_read_frame(ctx)
-	C.ffctx_free_packet(ctx)
+	C.av_register_all()
 }
-
 
